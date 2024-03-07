7d22b210f50972d670fa82c054566443ca5d29e5
Hai Tran <159185497+hai-tran-2@users.noreply.github.com>
2024-03-07 19:46:37 +0000

    added layout

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Hai Tran
+// Created on: March 2024
 // This file contains the JS functions for index.html
+
