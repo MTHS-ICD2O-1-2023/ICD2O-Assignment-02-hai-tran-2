9f507f0106e426fe3a6b7f848bf195f8275fc09c
Hai Tran <159185497+hai-tran-2@users.noreply.github.com>
2024-03-24 21:43:00 +0000

    added js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
+
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Hai Tran
 // Created on: March 2024
 // This file contains the JS functions for index.html
 
+"use strict"
+
+/**
+ * This function calculates area of a triangle.
+ */
+function calculateVolumeOfCylinder() {
+  // input
+  const heightOfCylinder = parseInt(document.getElementById('height-of-cylinder').value)
+  const radiusOfBase = parseInt(document.getElementById('radius-of-base').value)
+
+  // process
+  const volumeOfCylinder = (Math.PI * radiusOfBase * radiusOfBase * heightOfCylinder) 
+  // output
+  document.getElementById('answer').innerHTML = 'The volume is: ' + volumeOfCylinder.toFixed(2) + ' cm³'
+}
